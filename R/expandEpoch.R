9a93cb289f56edfaee3ab82791151d67b3fb2459
John Sakaluk <johnsakaluk@Johns-MacBook-Pro.local>
2023-04-13 14:36:42 -0400

    fix: expandEpoch functioning correctly (limited to 30 seconds for now) docs: update DESCRIPTION and pkgdown

--- R/expandEpoch.R
+++ R/expandEpoch.R
@@ -1,7 +1,7 @@
 #' Converts scrapePerson data w/ raw = TRUE into 30 or 60 epoch data
 #'
 #' @param data data frame from within scrapePerson with raw = TRUE
-#' @param epoch whether 30 or 60 epoch is requested
+#' @param epoch currently only 30 is supported
 #'
 #' @return data frame converted to 30 or 60 second epoch
 #'
@@ -9,74 +9,32 @@ expandEpoch <- function(data, epoch){
 
   if(epoch == 30){
     ##Add a unique "epoch" ID for each row
-    #Note that this is currently just the row number, so it won't be unique across data files
-
-    #data$epochID <- paste0(data$id, "_", seq.int(nrow(data)))
     data$eventID <- seq.int(nrow(data))
     eventNum <-  length(unique(data$eventID))
 
     ##Duplicate each row "seconds/30" number of times
     data30sec <- tidyr::uncount(data, .data$seconds/30)
 
+    #create dfEpoch
     dfEpoch <- data30sec
 
     # Sequentially count observations within each level of clustering variable
-    dfEpoch$eventObsID <- ave(dfEpoch$days_dif, dfEpoch$eventID, FUN = seq_along)
+    dfEpoch$eventObsID <- stats::ave(dfEpoch$days_dif, dfEpoch$eventID, FUN = seq_along)
     dfEpoch$obsID <- as.numeric(rownames(dfEpoch))
 
     dfEpoch <- dfEpoch %>%
-      ###Add a datetime_seq variable that starts with the original
-      #datetime value and increases in 30 second increments
-
-      #Group by epoch
+      #Group by eventID
       dplyr::group_by(.data$eventID) %>%
-      #convert dateTime to date-time (from character) %>% (no longer necessary)
-      #dplyr::mutate(dateTime = as.POSIXct(.data$dateTime)) %>%
-      ##Number of seconds to be added
-      dplyr::mutate(eventObsEndDateTime = (lubridate::seconds(30)*dplyr::row_number())+.data$eventDateTime)
-      ##Time when current 30-second increment began
-      #dplyr::mutate(eventTime_seq = .data$eventTime + .data$seconds_seq)
-  } else if(epoch == 60){
-    #data$epochID <- paste0(data$id, "_", seq.int(nrow(data)))
-    data$epochID <- seq.int(nrow(data))
+      ##create eventObsEndDateTime
+      dplyr::mutate(eventObsEndDateTime = (lubridate::seconds(30)*(dplyr::row_number()-1))+.data$eventDateTime)
 
-    ##Duplicate each row "seconds/60" number of times
-    data60sec <- tidyr::uncount(data, round(.data$seconds/60))
-
-
-    dfEpoch <- data60sec %>%
-
-      ###Add a datetime_seq variable that starts with the original
-      #datetime value and increases in 30 second increments
-
-      #Group by epoch
-      dplyr::group_by(.data$epochID) %>%
-      #convert dateTime to date-time (from character) %>% (no longer necessary)
-      #dplyr::mutate(dateTime = as.POSIXct(.data$dateTime)) %>%
-      ##Number of seconds to be added
-      dplyr::mutate(seconds_seq = 60*(dplyr::row_number()-1)) %>%
-      ##Time when current 60-second increment began
-      dplyr::mutate(eventTime_seq = .data$eventTime + .data$seconds_seq)
-
-    #####Collapse back down to 60-second cases (each row represents 60 seconds)
-
-    #dfEpoch <- dfEpoch %>%
-
-      #When a 30-second "sleep" or "wake" event is isolated/sandwiched, recode
-      #dplyr::ungroup() %>%
-      #dplyr::arrange(.data$eventTime_seq) %>% toggle for test
-      #dplyr::rename(sleepWake30 = .data$sleepWake) %>%
-      #dplyr::mutate(sleepWake = dplyr::case_when(dplyr::lag(.data$sleepWake30)=="sleep" & dplyr::lead(.data$sleepWake30)=="sleep" ~ "sleep",
-      #                                           dplyr::lag(.data$sleepWake30)=="wake" & dplyr::lead(.data$sleepWake30)=="wake" ~ "wake",
-      #                                           TRUE ~ sleepWake30)) %>%
-
-      #Drop all rows that begin at the 30 second mark (1 minute intervals only)
-      #dplyr::mutate(seconds_end = lubridate::second(.data$eventTime_seq)) %>%
-      #dplyr::filter(.data$seconds_end == 0) %>%
-      #Drop unneeded column
-      #dplyr::select(-c(.data$sleepWake30,.data$seconds_end))
-  }else{
-    stop("epoch must be either 30 or 60")
+    #retain cases of shortdata over data when at same eventObsEndDateTime
+    dfEpoch <- dfEpoch %>%
+      dplyr::group_by(.data$eventObsEndDateTime) %>%
+      dplyr::slice(which.max(source == "shortdata"))
+  }
+else{
+    stop("epoch must be 30 (for now)")
   }
 
   return(dfEpoch)
